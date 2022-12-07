743fef8e09bb47725c51800b5b563ea7403d1963
neggor <jordialg97@gmail.com>
2022-12-07 11:50:39 +0100

    Network improved

---
+++ app/src/main/java/com/example/digitsreader/Basics.kt
@@ -0,0 +1,10 @@
+package com.example.digitsreader
+
+fun main(){
+   val numbers: IntArray = intArrayOf(1, 2, 3, 4)
+
+   //print(numbers.contentToString())
+   for(element in numbers){
+      println(element)
+   }
+}
